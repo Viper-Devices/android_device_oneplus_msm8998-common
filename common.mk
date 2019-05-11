733d64c8bb3d77d81fce148e0e66baf1996af350
EsromTech <esromgalaxy5@gmail.com>
2019-05-10 21:44:56 -0300

    msm8998: build ThermalController feature

--- common.mk
+++ common.mk
@@ -143,6 +143,10 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     GCam
 
+# ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController
+
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
